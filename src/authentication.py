0d712fc5b6bc07e14f2eaf9895ea9d831a9cf29c
amf5214 <austinfraley.lee@gmail.com>
2023-12-26 04:12:56 -0500

    Created function to check if user has admin permission
    Created function to check if user has admin permission

--- src/authentication.py
+++ src/authentication.py
@@ -174,3 +174,18 @@ def verify_account_match(request, accountId):
     account = get_account(request)
     return account.id == accountId
 
+def check_if_admin(request):
+    """Validates user admin permissions
+
+    Takes in an http request and uses it to access the user auth token to see if the user has admin permissions
+
+    Keyword Arguements:
+    request -- http request containing a cookie named token which contains an authentication token
+
+    Return: boolean
+    """
+
+    account = get_account(request)
+    if account.full_name != "No Account":
+        return permission_validation("Admin", account.id)
+    
