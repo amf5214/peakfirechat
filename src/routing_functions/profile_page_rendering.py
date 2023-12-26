c7c8d6cf77464b6493bbb2f11b543420410796eb
amf5214 <austinfraley.lee@gmail.com>
2023-12-26 04:36:42 -0500

    Created view function to handle profile page
    Created view function to handle profile page

--- src/routing_functions/profile_page_rendering.py
+++ src/routing_functions/profile_page_rendering.py
@@ -35,3 +35,33 @@ class ProfilePageRendering():
 
         return render_template('signinup.html', message="Username/Password Invalid. Please try again.", useraccount=get_account(request))
 
+    def profile():
+        """View function that renders the profile page
+
+        Renders the page used to view user profile. Checks to ensure that there is a user signed in and 
+        redirects to sign in page if no user is signed in
+
+        Return: Rendered view from profile.html template with js and css code 
+
+        """
+
+        account = get_account(request)
+        if account.full_name != "No Account":
+            permissions = db.session.execute(db.select(AccountPermission).filter_by(account_id=account.id)).scalars()
+            permissions_gen = []
+            remaining_permissions = [z for z in Permission_values]
+            for x in permissions:
+                perm_type = x.permission_type
+                permissions_gen.append(Permission(has=True, name=perm_type))
+                remaining_permissions.remove(perm_type)
+            for y in remaining_permissions:
+                permissions_gen.append(Permission(has=False, name=y))
+            return render_template("profile.html", useraccount=account, permissions=permissions_gen)
+        else:
+            return redirect('/signin/home')
+
+# @app.route('/profile/introduction')
+# def introduction():
+#     return render_template("introduction.html", useraccount=get_account(request))
+
+
