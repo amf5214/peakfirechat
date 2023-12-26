b091a5a8fe5f00f34e28700aaa54ed4366ff3fcd
amf5214 <austinfraley.lee@gmail.com>
2023-12-26 04:46:10 -0500

    Created function to retrieve id values for an html element
    Created function to retrieve id values for an html element on the profile page

--- static/script/profile_modification.js
+++ static/script/profile_modification.js
@@ -1,2 +1,18 @@
 
 let userAttributes = document.getElementsByClassName("account-attribute");
+
+function getIdValues(element) {
+    let elementId = element.id;
+    let parts = elementId.split("-");
+    let newValueRaw = element.innerText;
+    let newValueData = newValueRaw.split(":");
+    let newValue = null;
+    if(newValueData.length > 1) {
+        newValue = newValueData.at(1).trim();
+    }
+    else {
+        newValue = newValueData.at(0).trim();
+    }
+    return {attribute: parts.at(1), accountId: parseInt(parts.at(2)), newValue: newValue}
+}
+
