c0ce101bb6d0c6112feaf61a0d0eee922514dd77
amf5214 <austinfraley.lee@gmail.com>
2023-12-26 04:21:10 -0500

    Created function to get all groups a user is in
    Created function to get all groups a user is in

--- src/messaging_functions.py
+++ src/messaging_functions.py
@@ -4,3 +4,8 @@ from sqlalchemy.orm.exc import NoResultFound
 from src.authentication import *
 from src.image_handling import *
 from src.models import AuthAccount, UserAccount, MessageGroupSubscription, Message, MessageGroup, db
+
+def get_users_group_subs(user_id):
+    subscriptions = db.session.execute(db.select(MessageGroupSubscription).filter_by(subscriber=user_id)).scalars()
+    return [x for x in subscriptions]
+
