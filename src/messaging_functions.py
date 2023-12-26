ddfa6bd4604c68c1c28bee264c451b903c6a1286
amf5214 <austinfraley.lee@gmail.com>
2023-12-26 04:21:28 -0500

    Created function to get all messages in a group
    Created function to get all messages in a group

--- src/messaging_functions.py
+++ src/messaging_functions.py
@@ -9,3 +9,19 @@ def get_users_group_subs(user_id):
     subscriptions = db.session.execute(db.select(MessageGroupSubscription).filter_by(subscriber=user_id)).scalars()
     return [x for x in subscriptions]
 
+def get_messages_by_group(group_id):
+    messages = db.session.execute(db.select(Message).filter_by(group=group_id)).scalars()
+    messages_sorted = sorted(messages, key=lambda x: x.datetime_sent)
+    messages_sent = [x for x in messages_sorted]
+    for message in messages_sent:
+        senders = db.session.execute(db.select(UserAccount).filter_by(id=message.sender)).scalars()
+        if senders:
+            senders = [x for x in senders]
+            if senders[0].full_name != None:  
+                message.sender_name = senders[0].full_name
+            else:
+                message.sender_name = senders[0].username
+        else:
+            message.sender_name = message.sender
+    return messages_sent
+
