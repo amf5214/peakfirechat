8a78a0a2cba8619fb58b926b15fd3dd923f69124
amf5214 <austinfraley.lee@gmail.com>
2023-12-26 04:41:04 -0500

    Created function to load permissions request page
    Created function to load permissions request page

--- src/routing_functions/admin_page_rendering.py
+++ src/routing_functions/admin_page_rendering.py
@@ -6,3 +6,10 @@ from src.authentication import *
 from src.image_handling import *
 
 class AdminPageRendering():
+   
+    def permissions_requests_admin():
+        if not check_if_admin(request):
+            return redirect('/')
+        requests = PermissionsRequest.query.filter_by(is_visible=True).order_by(PermissionsRequest.id).all()
+        return render_template("permissions_request_admin.html", admin_token=True, prequests=requests, useraccount=get_account(request))
+
