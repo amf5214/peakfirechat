fed62476bbcf4137c2e758f07c340066eb2c4de0
amf5214 <austinfraley.lee@gmail.com>
2023-12-26 04:18:24 -0500

    Created function to upload an item to the upload directory
    Created function to upload an item to the upload directory

--- src/image_handling.py
+++ src/image_handling.py
@@ -16,6 +16,29 @@ from sqlalchemy import create_engine
 import pymysql
 
 from src.models import FileContent, db
+def save_item(request):
+        """Function to save a file in the local directory
+
+        Accepts a post request in the form of an html form submission. It is important that the form has
+        enctype="multipart/form-data" activated so that you can access the files attribute. There should be
+        a file input with the name "file" which is what will be accessed
+        
+        Keyword Arguements:
+        request -- http post request from an html form submission
+
+        Return: string representing the name of the file
+
+        """
+        
+        user_file = request.files["file"]
+        if user_file.filename == '':
+            return None
+        if user_file:
+            filename = secure_filename(user_file.filename)
+            pic_name = str(uuid.uuid1()) + "_" + filename
+            user_file.save(os.path.join(app.config["ITEM_FOLDER"], pic_name))
+            return pic_name
+
 def render_picture(data):
     """Converts byte data for image into a viewable format that is safe for browsers
 
